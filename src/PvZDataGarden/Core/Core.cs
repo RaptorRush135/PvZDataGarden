37d4fc058538beb3daafd2d4c0007e7dd9379749
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-10-31 02:54:44 -0600

    chore: configure environment

--- src/PvZDataGarden/Core/Core.cs
+++ src/PvZDataGarden/Core/Core.cs
@@ -6,6 +6,6 @@ public sealed class Core : MelonMod
 {
     public override void OnInitializeMelon()
     {
-        LoggerInstance.Msg("Initialized.");
+        this.LoggerInstance.Msg("Initialized.");
     }
 }
