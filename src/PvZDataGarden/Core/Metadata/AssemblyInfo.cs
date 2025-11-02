a79986df75eeec74954ba95e4405b78ece65a798
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-11-02 11:02:28 -0600

    feat: add repo as download link

--- src/PvZDataGarden/Core/Metadata/AssemblyInfo.cs
+++ src/PvZDataGarden/Core/Metadata/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using MelonLoader;
 using PvZDataGarden;
 using PvZDataGarden.Metadata;
 
-[assembly: MelonInfo(typeof(Core), ModInfo.Name, ModInfo.Version, ModInfo.Author)]
+[assembly: MelonInfo(typeof(Core), ModInfo.Name, ModInfo.Version, ModInfo.Author, ModInfo.DownloadLink)]
 [assembly: MelonGame("PopCap Games", "PvZ Replanted")]
 [assembly: MelonColor(255, 120, 220, 50)]
